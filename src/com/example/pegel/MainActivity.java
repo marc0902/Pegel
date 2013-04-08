7356be256cadb6287a6012dd3e8cb25adfa32c22
Albert <tuttut@mailinator.com>
2013-04-08 19:29:49 +0200

    Taste ruft entsprechende Webseite auf

--- src/com/example/pegel/MainActivity.java
+++ src/com/example/pegel/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pegel;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +21,17 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	public void tasteklick(View view){
+		  goToUrl ( "http://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/ccccb57f-a2f9-4183-ae88-5710d3afaefd/W/currentmeasurement.json"); 
+		  
+		 }
+		 
+		 
+		 private void goToUrl (String url) {
+		        Uri uriUrl = Uri.parse(url);
+		        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+		        startActivity(launchBrowser);
+		         }
 
 }
